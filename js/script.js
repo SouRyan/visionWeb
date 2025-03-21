a96cd5c8e5a549ef1cd5937a8378ab767cabca67
Ryan Brayan <ryanbrayan.fs@gmail.com>
2025-03-21 16:53:15 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+var typed = new Typed('#element', {
+  strings: ['<i>Sua Empressa</i>', '<i>Seu Negócio</i>', '<i>Seu Projeto</i>','<i>Sua Presença Online</i>', '<i>Seu Sucesso</i>' ,'<i>Para VOCÊ!</i>'],
+  typeSpeed: 50,
+  backSpeed: 60,
+  loop: true
+});
+
+
