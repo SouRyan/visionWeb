6ee1094d6b159c216f7fa66d40720d838cc53a93
RYAN BRAYAN FRANCA SOUZA <ryan.bfsouza@senacsp.edu.br>
2025-03-24 22:09:52 -0300

    aula 20.03

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 
-
 var typed = new Typed('#element', {
-  strings: ['<i>Sua Empressa</i>', '<i>Seu Negócio</i>', '<i>Seu Projeto</i>','<i>Sua Presença Online</i>', '<i>Seu Sucesso</i>' ,'<i>Para VOCÊ!</i>'],
+  strings: ['<i>Sua Empressa</i>', '<i>Seu Negócio</i>', '<i>Seu Projeto</i>', '<i>Sua Presença Online</i>', '<i>Seu Sucesso</i>', '<i>Para VOCÊ!</i>'],
   typeSpeed: 50,
   backSpeed: 60,
   loop: true
 });
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+  // loopAdditionalSlides: 1,  // Isso cria um slide adicional para garantir a transição suave
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
